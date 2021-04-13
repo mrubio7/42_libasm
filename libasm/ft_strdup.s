6ef2c4534b619f1b774c4b0023068833055f4d63
Manuel Rubio <manuel.rubio.arias@gmail.com>
2021-04-13 20:41:25 +0200

    ft_strdup

--- libasm/ft_strdup.s
+++ libasm/ft_strdup.s
@@ -4,11 +4,11 @@ segment .text
 	extern _ft_strlen
 
 	_ft_strdup:
-		mov rbx, rdi
-		call _ft_strlen
-		mov edi, eax
-		call _malloc
-		mov rax, rbx
+		mov rbx, rdi		; Pasamos el arg1 (str) al rbx
+		call _ft_strlen		; Llamamos a ft_strlen que se guarda en eax
+		mov edi, eax		; Pasamos eax a edi por que malloc lo lea
+		call _malloc		; Llamamos a malloc que usa edi para saber cuanto asignar
+		mov rax, rbx		; Pasamos rbx(str) al registro de salida
 		jmp ret
 
 	ret:
