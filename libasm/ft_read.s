76cc31c7a829b5bbb3f2c7a3b5df8a168f9b7f35
Manuel Rubio <manuel.rubio.arias@gmail.com>
2021-04-15 19:47:27 +0200

    write and read works

--- libasm/ft_read.s
+++ libasm/ft_read.s
@@ -1,7 +1,16 @@
 segment .text
-	global _ft_read
+global _ft_read
+extern	__errno_location
 
 	_ft_read:							; fd(rdi), buffer(rsi), nbytes(rdx)
-		mov rax, 0x200005				; function read -> syscall = 0x2000005 (macos) // 0x0 (linux)
+		mov 	rax, 0x2000003			; function read -> syscall = 0x2000003 (macos) // 0x0 (linux)
 		syscall
-		ret
+		jc error_return					; jump error_return if cant use read
+		jmp exit
+
+	error_return:
+		mov rax, -1
+		ret								; return
+	
+	exit:
+		ret								; return 
--- libasm/ft_write.s
+++ libasm/ft_write.s
@@ -4,4 +4,12 @@ segment .text
 	_ft_write:							; fd(rdi), buffer(rsi), nbytes(rdx)
 		mov rax, 0x200004				; function write -> syscall = 0x2000004 (macos) // 0x1 (linux)
 		syscall
+		jc error_return					; jump error_return if cant use write
+		jmp exit
+
+	error_return:
+		mov rax, -1
+		ret
+
+	exit:
 		ret
